31320cd9823fdfb98fd49dcedb50dcc7ed485721
Tanner Davison <Tboydavison@gmail.com>
2024-10-16 00:12:20 -0600

    function pointers with arrays

--- Callables_Cplusplus/Callables_Cplusplus.cpp
+++ Callables_Cplusplus/Callables_Cplusplus.cpp
@@ -33,10 +33,26 @@ int main()
 
 	void(*dis)(int x) = display2;
 	void(*dis1)(int x) = display;
+
 	const int ARRSIZE = 5;
 	int scores[ARRSIZE] = { 34,52,81 };
+
 	printScores(scores, ARRSIZE, dis1);
 	printScores(scores, ARRSIZE, dis);
+
+	//array of function pointers
+
+	int(*fp_arr[])(int, int) = { add, multiply };
+	int x{}, y{}, choice{};
+
+	std::cout << "Enter 2 Numbers";
+	std::cin >> x >> y;
+
+	std::cout << "Enter 0 to add, 1 to multiply";
+	std::cin >> choice;
+
+	std::cout << (*fp_arr[choice])(x, y) << std::endl;
+
 	return EXIT_SUCCESS;
 }
 
